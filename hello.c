ddecf12d0a3c2994d712d4220679927246cc5f7c
Pol Agullo <pol.agullo@gygroup.com>
2026-04-01 14:30:44 +0200

    Hello, raylib!

--- hello.c
+++ hello.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
+#include <raylib.h>
+
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a < b ? a : b)
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#define TITLE_FONT_SIZE 60
+
 int main() {
     printf("Hello, World!\n");
+
+    InitWindow(800, 600, "Hello, World!");
+    
+    while (!WindowShouldClose()) {
+    BeginDrawing();
+
+        ClearBackground(RED);
+
+        // void DrawText(const char *text, int posX, int posY, int fontSize, Color color);
+        DrawText(TextFormat("FPS: %i", GetFPS()), 0, 0, 20, BLACK);
+        DrawText("Hello, Raylib!", 200, (SCREEN_HEIGHT / 2) - (TITLE_FONT_SIZE / 2), TITLE_FONT_SIZE, BLACK);
+
+    EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
--- nob.c
+++ nob.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 
 #define NOB_IMPLEMENTATION
-#define NOB_STRIP_PREFIXES
-#include "nob.h"
+#include "includes/nob.h"
 
 #include <stdint.h>
 typedef int32_t i32;
-
-#if defined(__TINYC__)
-#   define CC "tcc"
-#elif defined(__GNUC__)
-#   define CC "gcc"
-#endif // __TINYC__
-
-#ifdef _WIN32
-#   define BIN_PATH "hello.exe"
-#else
-#   define BIN_PATH "hello.out"
-#endif // _WIN32
 
 i32 main(i32 argc, char** argv) {
     GO_REBUILD_URSELF(argc, argv);
     
     Cmd cmd = {0};
 
-    cmd_append(&cmd, CC, "-o", BIN_PATH, "hello.c");
+    cmd_append(&cmd, "gcc");
+    cmd_append(&cmd, "-o", "hello.exe", "hello.c");
+    cmd_append(&cmd, "-Iincludes");
+    cmd_append(&cmd, "-Llib");
+    cmd_append(&cmd, "-l:libraylib.a");
+    cmd_append(&cmd, "-lgdi32", "-lwinmm");
     cmd_run(&cmd);
     
     return 0;
