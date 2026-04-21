c28e860cf4634650b36d4e58cd9a9e1266e09704
Pol Agullo <pol.agullo@gygroup.com>
2026-04-21 15:11:49 +0200

    Cross-platform compatibility

--- nob.c
+++ nob.c
@@ -6,17 +6,25 @@
 #include <stdint.h>
 typedef int32_t i32;
 
+#define CC "gcc"
+
+#if defined(_WIN32)
+#   define OUT_PATH "hello.exe"
+#   define SYS_LIBS "-lgdi32", "-lwinmm"
+#else
+#   define OUT_PATH "hello.out"
+#   define SYS_LIBS "-lm"
+#endif // defined(_WIN32)
+
 i32 main(i32 argc, char** argv) {
     GO_REBUILD_URSELF(argc, argv);
     
     Cmd cmd = {0};
 
-    cmd_append(&cmd, "gcc");
-    cmd_append(&cmd, "-o", "hello.exe", "hello.c");
-    cmd_append(&cmd, "-Iincludes");
-    cmd_append(&cmd, "-Llib");
-    cmd_append(&cmd, "-l:libraylib.a");
-    cmd_append(&cmd, "-lgdi32", "-lwinmm");
+    cmd_append(&cmd, CC);
+    cmd_append(&cmd, "-o", OUT_PATH, "hello.c");
+    cmd_append(&cmd, "-Iincludes", "-Llib", "-l:libraylib.a");
+    cmd_append(&cmd, SYS_LIBS);
     cmd_run(&cmd);
     
     return 0;
