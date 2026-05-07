fb06347a854497984dd7212a9f038ce8cee6d3a3
Eles <polagullogarcia@gmail.com>
2026-05-07 21:59:03 +0200

    Colored mandelbrot set

--- shaders/mandelbrot.fs
+++ shaders/mandelbrot.fs
@@ -26,8 +26,13 @@ void main() {
         if ((p.x * p.x + p.y * p.y) > 4) break;
     }
 
-    float color = clamp(float(i) / (maxIter/4), 0, 1);
+    float color = clamp(float(i) / (maxIter/3), 0, 1);
     if ((p.x * p.x + p.y * p.y) < 4) color = 0;
 
-    pixelColor = vec4(color, color, color, 1);
+    pixelColor = vec4(
+            color * 2 - 1,
+            color * 2 - 1, 
+            min(color * 2, 1),
+            1
+    );
 }
